528e50125833f332a624fd9e3e0f95b56fe46f18
deskent <battenetciz@gmail.com>
2024-07-05 09:45:07 +0300

    add parse aws params

--- src/runner.py
+++ src/runner.py
@@ -1,6 +1,6 @@
 import argparse
 import subprocess
-from pathlib import Path
+import sys
 from subprocess import CompletedProcess
 
 from .config import logger, LOGS_DIR
@@ -25,22 +25,66 @@ class DockerRunner:
             type=str,
             help="Bash command to be executed",
         )
+        parser.add_argument(
+            "--aws-cloudwatch-group",
+            required=False,
+            action="store",
+            type=str,
+            help="AWS CloudWatch group",
+        )
+        parser.add_argument(
+            "--aws-cloudwatch-stream",
+            default=None,
+            required=False,
+            action="store",
+            type=str,
+            help="AWS CloudWatch stream",
+        )
+        parser.add_argument(
+            "--aws-access-key-id",
+            default=None,
+            required=False,
+            action="store",
+            type=str,
+            help="AWS CloudWatch access key",
+        )
+        parser.add_argument(
+            "--aws-secret-access-key",
+            default=None,
+            required=False,
+            action="store",
+            type=str,
+            help="AWS CloudWatch secret access key",
+        )
+        parser.add_argument(
+            "--aws-region",
+            default=None,
+            required=False,
+            action="store",
+            type=str,
+            help="AWS CloudWatch region",
+        )
 
         return parser.parse_args()
 
     def _run_docker(
         self,
         command: str,
-        output_logs_file: Path,
-        output_errors_file: Path,
+        stderr,
+        stdout,
     ) -> 'CompletedProcess':
         """Run docker with parameters."""
+
+        logger.info(
+            f'\n\n\t\tRunning docker command: {command}'
+            f'\nError output: {stderr}\nStdout: {stdout}'
+        )
 
         result: 'CompletedProcess' = subprocess.run(
             args=[command],
             shell=True,
-            stderr=open(output_errors_file, 'a', encoding='utf-8'),
-            stdout=open(output_logs_file, 'a', encoding='utf-8'),
+            stderr=stderr,
+            stdout=stdout,
         )
 
         return result
@@ -52,9 +96,40 @@ class DockerRunner:
 
         output_logs_file = LOGS_DIR / 'docker.logs'
         output_errors_file = LOGS_DIR / 'docker_errors.logs'
+        stderr = open(output_errors_file, 'a', encoding='utf-8')
+        stdout = open(output_logs_file, 'a', encoding='utf-8')
 
         command: str = (
-            f'docker run {params.docker_image} {params.bash_command}'
+            f'docker run {params.docker_image} {params.bash_command} '
         )
 
-        return self._run_docker(command, output_logs_file, output_errors_file)
+        driver: str | None = None
+        if params.aws_cloudwatch_group is not None:
+            driver = 'awslogs'
+        if params.aws_cloudwatch_stream is not None:
+            driver = 'awslogs-stream'
+
+        if all(
+            (
+                driver,
+                params.aws_access_key_id,
+                params.aws_secret_access_key,
+                params.aws_region,
+            )
+        ):
+            command += (
+                f'--log-driver={driver} '
+                f'--log-opt awslogs-region={params.aws_region} '
+                f'--log-opt awslogs-group={params.aws_cloudwatch_group} '
+                f'--log-opt awslogs-create-group=true'
+                f'--env AWS_ACCESS_KEY_ID={params.aws_access_key_id} '
+                f'--env AWS_SECRET_ACCESS_KEY={params.aws_secret_access_key} '
+            )
+            stderr = sys.stderr
+            stdout = sys.stdout
+
+        return self._run_docker(
+            command,
+            stderr=stderr,
+            stdout=stdout,
+        )
--- test.sh
+++ test.sh
@@ -1,11 +1,14 @@
+#!/usr/bin/bash
+
+export $(cat ./.env)
+
 BASH_COMMAND=$'pip install pip -U && pip install tqdm && python -c \"import time\ncounter = 0\nwhile True:\n\tprint(counter)\n\tcounter = counter + 1\n\ttime.sleep(0.1)\"'
 
 python main.py \
   --docker-image python \
-  --bash-command "${BASH_COMMAND}"
-#   \
-#  --aws-cloudwatch-group test-task-group-1 \
-#  --aws-cloudwatch-stream test-task-stream-1 \
-#  --aws-access-key-id ... \
-#  --aws-secret-access-key ... \
-#  --aws-region ...
+  --bash-command "${BASH_COMMAND}" \
+  --aws-cloudwatch-group test-task-group-1 \
+  --aws-cloudwatch-stream test-task-stream-1 \
+  --aws-access-key-id "${AWS_ACCESS_KEY_ID}" \
+  --aws-secret-access-key "${AWS_SECRET_ACCESS_KEY}" \
+  --aws-region "${AWS_DEFAULT_REGION}"
